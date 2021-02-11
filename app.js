a95f3764dae728a981e891e3033b77a8e6065b1d
ahmad shalan <engashalan@gmail.com>
2021-02-12 00:14:03 +0200

    problem5

--- app.js
+++ app.js
@@ -164,15 +164,22 @@ testMultiplyArray(testArray);
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
-Write a function called multiplyAnyArray() that takes an array of numbers of any length as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called multiplyAnyArray() that takes an array of numbers of any length as 
+its argument and returns an array whose first element is the product of those numbers, and 
+the second element is a string that EXACTLY follows this example and concatenates a message
+ using the arguments that were passed into the function:
 
 "The numbers 1,2,3,4,5 have a product of 120."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. 
+To do multiplication, use your multiply() function that you've already created. 
+You're going to have to be resourceful to figure out how to do this. However, you may
+ continue to use the + operator for string concatenation.
 
 This function should be dynamic, accepting an array of any length.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is
+ finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
 let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
